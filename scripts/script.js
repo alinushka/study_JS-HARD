a1fe844d4c80ecd8ee5f8ea84f9743bdfecf852a
alin.ushka <salatbulka@mail.ru>
2021-06-16 20:36:27 +0400

    создала новую ветку

--- scripts/script.js
+++ scripts/script.js
@@ -1,22 +1,5 @@
 'use strict';
 
-let money = 5500;
-let income = 'фриланс';
-let addExpenses = 'Интернет, Такси, Коммуналка';
-let deposit = true;
-let mission = 150000;
-let period = 6;
+let num = 266219;
 
-console.log('money =', typeof money);
-console.log('income =', typeof income);
-console.log('deposit =', typeof deposit);
-
-console.log(addExpenses.length);
-
-console.log('\"Период равен ' + period + ' месяцев\"' + ' и ' + '\"Цель заработать ' + mission + ' рублей\"');
-
-let massiv = addExpenses.toLowerCase();
-console.log(massiv.split(', '));
-
-let budgetDay = money / 30;
-console.log('budgetDay: ', budgetDay);
+for (let)
