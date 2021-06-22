f4f9f60fcf100b1798b20a683975a0bf9135d71f
alin.ushka <salatbulka@mail.ru>
2021-06-22 14:43:07 +0400

    done

--- scripts/script.js
+++ scripts/script.js
@@ -1,23 +1,3 @@
 "use strict";
 
 let num = 123;
-
-// while (Math.abs(num) > 0) {
-//   let gg = mult * (num % 10);
-//   num = num / 10;
-//   console.log(num);
-// }
-
-// console.log(gg);
-
-const user = {
-  name: "Василий",
-  ageYear: 1990,
-  online: true,
-};
-
-if (user.name === "Василий" && user.ageYear === 1990) {
-  console.log(user.name + " админ");
-} else {
-  console.log(user.name + " пользователь");
-}
