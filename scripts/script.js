d30d6e70b28c9d164c6b6299d6e8db803e1cea38
alin.ushka <salatbulka@mail.ru>
2021-06-23 19:46:19 +0400

    готово

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,13 @@
 "use strict";
 
-let num = 123;
+const num = 266219;
+const strNum = num.toString();
+
+let mult = 1;
+for (let i = 0; i < strNum.length; i++) {
+  mult *= strNum[i];
+}
+console.log('переумножение: ', mult);
+
+const n = mult ** 3;
+console.log('первые 2 цифры числа, возведенного в 3 степень: ', n.toString().substring(0, 2));
